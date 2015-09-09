172a6ae63958d3eccf55e4e212c09f89c9a981c4
pan <1028353676@qq.com>
2015-09-09 12:58:04 +0800

    ...

--- src/com/novel/util/CatchTianyaTask.java
+++ src/com/novel/util/CatchTianyaTask.java
@@ -3,9 +3,11 @@ package com.novel.util;
 import java.util.Date;
 
 import org.springframework.stereotype.Component;
-
+/**
+ *定时抓取天涯论坛 
+ */
 @Component
-public class TaskJob {
+public class CatchTianyaTask {
 	public void job1() {  
         System.out.println("任务进行中。。。" + new Date(System.currentTimeMillis()).toLocaleString());  
     }  
